c2af7864c2b88a92f78f41a63ef4a9df37b87d39
francesco-mauro <francesco.mauro180@gmail.com>
2024-05-20 18:14:53 +0200

    add: exercise end

---
+++ greetings.php
@@ -0,0 +1,19 @@
+<?php
+// GET
+$paragraph = $_GET['paragraph'];
+$badword = $_GET['badword'];
+
+// paragrafo originale
+echo "<h3>Paragrafo originale:</h3>";
+echo "<p>$paragraph</p>";
+echo "<p>Lunghezza: " . strlen($paragraph) . " caratteri</p>";
+
+// parola con ***
+$censored_paragraph = str_ireplace($badword, '***', $paragraph);
+
+// Stampa coon censura
+echo "<h3>Paragrafo censurato:</h3>";
+echo "<p>$censored_paragraph</p>";
+echo "<p>Lunghezza: " . strlen($censored_paragraph) . " caratteri</p>";
+?>
+
--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-<?php 
-$my_username = "Francesco";
-
-echo "Ciao " . $my_username;
+<?php
+// file che gestisce i dati del form 
 ?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Badwords</title>
+</head>
+
+<body>
+    <form action="greetings.php" method="get">
+        <h2>Paragrafo:</h2>
+        <textarea name="paragraph"></textarea>
+        <h2>Parola da censurare:</h2>
+        <input type="text" name="badword">
+        <button type="submit">Invia</button>
+    </form>
+</body>
+
+</html>
