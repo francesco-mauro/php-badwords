39158251e04a915779deb278ef91ed892de9c30b
francesco-mauro <francesco.mauro180@gmail.com>
2024-05-21 09:53:21 +0200

    add: style with bootstrap

--- index.php
+++ index.php
@@ -8,16 +8,37 @@
 <head>
     <meta charset="UTF-8">
     <title>PHP Badwords</title>
+    <!--  Bootstrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <form action="greetings.php" method="get">
-        <h2>Paragrafo:</h2>
-        <textarea name="paragraph"></textarea>
-        <h2>Parola da censurare:</h2>
-        <input type="text" name="badword">
-        <button type="submit">Invia</button>
-    </form>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h2>PHP Badwords</h2>
+                    </div>
+                    <div class="card-body">
+                        <form action="greetings.php" method="get">
+                            <div class="form-group">
+                                <label for="paragraph"><h5>Paragrafo:</h5></label>
+                                <textarea class="form-control" name="paragraph" id="paragraph" rows="4"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="badword"><h5>Parola da censurare:</h5></label>
+                                <input type="text" class="form-control" name="badword" id="badword">
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block mt-3">Invia</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
+
